08a0abc1c093155dd0e669ca52652770abcabb96
Puyvong Heng <160134498+puyvongheng@users.noreply.github.com>
2025-06-13 17:18:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -230,4 +230,6 @@ def get_log():
         return jsonify(logs=logs)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get PORT from environment or default to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
+
